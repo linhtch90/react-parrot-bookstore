6ab5f61f49ed0e62c0f822f168db41131245e356
Linh Truong Cong Hong <linhtch90@gmail.com>
2020-10-21 22:03:12 +0700

    Updates

---
+++ precache-manifest.edd6048d4a6415ab4e54993d1535e392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbbebc9d8b5a0a667d3750bd3bf1369",
+    "url": "/react-parrot-restaurant/index.html"
+  },
+  {
+    "revision": "8ecc5ac1a1b5efd18d3f",
+    "url": "/react-parrot-restaurant/static/css/main.e5e7c6b1.chunk.css"
+  },
+  {
+    "revision": "61b2e4918c4f8228f1fc",
+    "url": "/react-parrot-restaurant/static/js/2.89d14bcb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-parrot-restaurant/static/js/2.89d14bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecc5ac1a1b5efd18d3f",
+    "url": "/react-parrot-restaurant/static/js/main.dfe0b8a0.chunk.js"
+  },
+  {
+    "revision": "7c4b8a1a308b475da434",
+    "url": "/react-parrot-restaurant/static/js/runtime-main.e3017073.js"
+  }
+]);
